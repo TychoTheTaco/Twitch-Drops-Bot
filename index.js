64cf9903f17831ef6c475bdba14316a396835a64
Tycho Bellers <tycho.bellers999@gmail.com>
2021-06-17 22:14:21 -0700

    Semi-functional.

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
-'use strict'
+'use strict';
 
-const axios = require('axios')
+const fs = require('fs');
+
+const twitch = require('./twitch');
 
 // Using puppeteer-extra to add plugins
 const puppeteer = require('puppeteer-extra');
@@ -9,52 +11,34 @@ const puppeteer = require('puppeteer-extra');
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 puppeteer.use(StealthPlugin());
 
-// Add Ad blocker
-//const AdBlockerPlugin = require('puppeteer-extra-plugin-adblocker')
-//puppeteer.use(AdBlockerPlugin())
-
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-async function watchStream(browser, cookies, url) {
-    console.log('watch stream')
-    const page = await browser.newPage();
-    //await page.setViewport({width: 1920, height: 1080})
-    await page.goto(url);
-    await sleep(10000)
-}
-
-async function getDropProgress(drop) {
-
-}
-
-async function getDropCampaigns(credentials) {
-    const response = await axios.post('https://gql.twitch.tv/gql',
-        {
-            'operationName': 'ViewerDropsDashboard',
-            'extensions': {
-                'persistedQuery': {
-                    "version": 1,
-                    "sha256Hash": "e8b98b52bbd7ccd37d0b671ad0d47be5238caa5bea637d2a65776175b4a23a64"
+async function getInventoryDrop(credentials, campaignId, dropId) {
+    const campaigns = await twitch.getDropCampaignsInProgress(credentials);
+    for (const campaign of campaigns){
+        if (campaign['id'] === campaignId){
+            const drops = campaign['timeBasedDrops'];
+            for (const drop of drops){
+                if (drop['id'] === dropId){
+                    return drop;
                 }
             }
-        },
-        {
-            headers: {
-                'Content-Type': 'text/plain;charset=UTF-8',
-                'Client-Id': credentials['client_id'],
-                'Authorization': `OAuth ${credentials['oauth_token']}`
-            }
         }
-    );
-    return response['data']['data']['currentUser']['dropCampaigns']
+    }
+    return null;
 }
 
-async function login(browser, username, password) {
-    const page = await browser.newPage();
-    await page.setViewport({width: 1920, height: 1080})
+async function getActiveDropCampaigns(credentials) {
+    const campaigns = await twitch.getDropCampaigns(credentials);
+    return campaigns.filter(campaign => {
+        return campaign['status'] === 'ACTIVE';
+    });
+}
+
+async function login(page, username, password) {
     await page.goto('https://www.twitch.tv/login');
 
     // Enter username
@@ -68,34 +52,244 @@ async function login(browser, username, password) {
     // Login
     await page.click('[data-a-target="passport-login-button"]');
     await page.waitForNavigation();
+}
 
-    // Save cookies so we don't have to login again
-    const cookies = await page.cookies();
+async function claimDrop(credentials, page, drop){
+    console.log('Claiming drop:', drop);
+    await twitch.claimDropReward(credentials, drop['self']['dropInstanceID']);
+}
 
-    // Close the tab
-    page.close();
+async function watchStreamUntilDropCompleted(page, streamUrl, twitchCredentials, campaign, drop){
+    await page.goto(streamUrl);
 
-    return cookies;
+    try {
+        // Click "Accept mature content" button
+        await page.click('[data-a-target="player-overlay-mature-accept"]');
+    } catch (error) {
+        // Ignore errors
+    }
+
+    await setLowestStreamQuality(page);
+
+    // Check for drop progress
+    let lastMinutesWatched = 0;
+    while (true){
+
+        // Check drop progress
+        const inventoryDrop = await getInventoryDrop(twitchCredentials, campaign['id'], drop['id']);
+        const currentMinutesWatched = inventoryDrop['self']['currentMinutesWatched'];
+        const requiredMinutesWatched = inventoryDrop['requiredMinutesWatched'];
+        console.log('Progress:', currentMinutesWatched, '/', requiredMinutesWatched, 'minutes.');
+
+        // Check if we have completed the drop
+        if (currentMinutesWatched >= requiredMinutesWatched){
+            return;
+        }
+
+        // Make sure drop progress has increased. If it hasn't, then something is wrong.
+        if (currentMinutesWatched <= lastMinutesWatched){
+            throw new Error('No progress made');
+        }
+
+        lastMinutesWatched = currentMinutesWatched;
+
+        // Sleep for a few minutes
+        await sleep(1000 * 60 * 2);
+    }
+}
+
+async function setLowestStreamQuality(page){
+    await page.click('[data-a-target="player-settings-button"]');
+    await page.click('[data-a-target="player-settings-menu-item-quality"]');
+    //await page.click('div[data-a-target="player-settings-menu"]>div:last-child input');
+    await page.evaluate(() => {  // This is a workaround for the commented line above, which causes "Node is either not visible or not an HTMLElement" error.
+        document.querySelector('div[data-a-target="player-settings-menu"]>div:last-child input').click();
+    });
+}
+
+async function isDropClaimed(credentials, drop){
+    const inventory = await twitch.getInventory(credentials);
+
+    // Check campaigns in progress
+    const dropCampaignsInProgress = inventory['dropCampaignsInProgress'];
+    for (const campaign of dropCampaignsInProgress){
+        for (const d of campaign['timeBasedDrops']){
+            if (d['id'] === drop['id']){
+                return d['self']['isClaimed'];
+            }
+        }
+    }
+
+    // Check claimed drops
+    const gameEventDrops = inventory['gameEventDrops'];
+    for (const d of gameEventDrops){
+        if (d['id'] === drop['benefitEdges'][0]['benefit']['id']){
+            return Date.parse(d['lastAwardedAt']) > Date.parse(drop['startAt']);
+        }
+    }
+
+    return false;
+}
+
+class NoStreamsError extends Error {
+
+}
+
+async function processCampaign(page, campaign, twitchCredentials){
+    // Get all drops for this campaign
+    const details = await twitch.getDropCampaignDetails(twitchCredentials, campaign['id']);
+    const drops = details['timeBasedDrops'];
+
+    for (const drop of drops){
+        console.log('DROP:', drop['name']);
+
+        // Check if we already claimed this drop
+        if (await isDropClaimed(twitchCredentials, drop)){
+            console.log('Drop already claimed');
+            continue;
+        }
+
+        const inventoryDrop = await getInventoryDrop(twitchCredentials, campaign['id'], drop['id']);
+
+        // Check if this drop is ready to claim
+        if (inventoryDrop['self']['currentMinutesWatched'] >= inventoryDrop['requiredMinutesWatched']){
+            await claimDrop(page, drop);
+            continue;
+        }
+
+        // Get list of valid streams
+        const streams = await twitch.getDropEnabledStreams(twitchCredentials, campaign['game']['displayName']);
+
+        // If there are no steams, try the next campaign
+        if (streams.length === 0){
+            throw new NoStreamsError();
+        }
+
+        // Watch first stream
+        try {
+            console.log('Drop:', inventoryDrop['name']);
+            await watchStreamUntilDropCompleted(page, streams[0], twitchCredentials, campaign, drop);
+        } catch (error) {
+            console.log('ERROR:', error);
+            break;
+        }
+
+        // Claim the drop
+        await claimDrop(page, drop);
+    }
 }
 
 (async () => {
 
-    // Start browser
-    const browser = await puppeteer.launch({headless: false});
+    // Load config file
+    const config = require('./config.json');
+
+    // Start browser and open a new tab. Headless mode DOES NOT WORK for Twitch AFKing
+    const browser = await puppeteer.launch({headless: false, executablePath: config['browser'], args: ['--mute-audio']});
+    const page = await browser.newPage();
 
     // Load Twitch credentials
-    const twitchCredentials = require('./credentials/twitch-test.json');
+    const twitchCredentials = require('./credentials/twitch-test.json'); // TODO: MUST DELETE COOKIES IF CHANGING ACCOUNT
 
-    // Login to Twitch
-    console.log('Logging in to Twitch...');
-    const cookies = await login(browser, twitchCredentials['username'], twitchCredentials['password']);
-    console.log('Login successful.');
+    // Seems to be the default hard-coded client ID
+    // Found in sources / static.twitchcdn.net / assets / minimal-cc607a041bc4ae8d6723.js
+    twitchCredentials['client_id'] = 'kimne78kx3ncx6brgo4mv6wki5h1ko';
 
-    // Update drop campaigns
-    const r = await getDropCampaigns(twitchCredentials);
-    console.log('RESPONSE\n', r)
+    // Check if we have saved cookies
+    const cookiesPath = './cookies.json';
+    if (fs.existsSync(cookiesPath)){
 
-    //await getDropCampaigns(browser, cookies);
+        // Restore cookies from previous session
+        console.log('Restoring cookies from last session.')
+        const cookies = JSON.parse(fs.readFileSync(cookiesPath, 'utf-8'));
+        await page.setCookie(...cookies);
 
-    await browser.close();
-})();
+    } else {
+
+        // Login to Twitch
+        console.log('Logging in to Twitch...');
+        await login(page, twitchCredentials['username'], twitchCredentials['password']);
+        console.log('Login successful.');
+
+        // Save cookies for next time
+        fs.writeFileSync(cookiesPath, JSON.stringify(await page.cookies()));
+    }
+
+    // Get some data from the cookies
+    for (const cookie of await page.cookies('https://www.twitch.tv')){
+        switch (cookie['name']){
+            case 'auth-token':  // OAuth token
+                twitchCredentials['oauth_token'] = cookie['value'];
+                break;
+
+            case 'persistent':  // "channelLogin" Used for "DropCampaignDetails" operation
+                twitchCredentials['channel_login'] = cookie['value'].split('%3A')[0];
+                break;
+
+            case 'login':
+                console.log('Logged in as', cookie['value']);
+                break;
+        }
+    }
+
+    while (true){
+
+        // Update drop campaigns
+        console.log('Updating drop campaigns...');
+        const campaigns = await getActiveDropCampaigns(twitchCredentials);
+        console.log('Found', campaigns.length, 'active campaigns.');
+
+        // Add to pending
+        const pending = [];
+        campaigns.forEach(campaign => {
+            if (config['games'].includes(campaign['game']['id'])) {
+                pending.push(campaign);
+            }
+        });
+        console.log('Found', pending.length, 'pending campaigns.')
+
+        while (pending.length > 0) {
+
+            // Get campaign from queue
+            const campaign = pending.pop();
+            console.log('Processing campaign:', campaign['game']['displayName'], campaign['name']);
+
+            // Make sure Twitch account is linked
+            if (!campaign['self']['isAccountConnected']){
+                console.warn('Twitch account not linked! Skipping this campaign.');
+                continue;
+            }
+
+            // Check campaign status
+            switch (campaign['status']){
+                case 'UPCOMING':
+                    console.log('This campaign is not active yet.');
+                    pending.push(campaign);
+                    continue;
+
+                case 'EXPIRED':
+                    console.log('This campaign has expired.');
+                    break;
+            }
+
+            try{
+                await processCampaign(page, campaign, twitchCredentials);
+            } catch (error){
+                if (error instanceof NoStreamsError){
+                    console.log('No streams!');
+                } else {
+                    console.log('Error:', error);
+                }
+            }
+        }
+
+        console.log('Sleeping for a bit...');
+        await sleep(1000 * 60 * 15);
+    }
+
+})().catch(error => {
+    console.log(error);
+    process.exit(1);
+}).finally(() => {
+    process.exit(0);
+});
---
+++ twitch.js
@@ -0,0 +1,163 @@
+'use strict';
+
+const axios = require('axios');
+
+async function getDropCampaigns(credentials) {
+    const response = await axios.post('https://gql.twitch.tv/gql',
+        {
+            'operationName': 'ViewerDropsDashboard',
+            'extensions': {
+                'persistedQuery': {
+                    "version": 1,
+                    "sha256Hash": "e8b98b52bbd7ccd37d0b671ad0d47be5238caa5bea637d2a65776175b4a23a64"
+                }
+            }
+        },
+        {
+            headers: {
+                'Content-Type': 'text/plain;charset=UTF-8',
+                'Client-Id': credentials['client_id'],
+                'Authorization': `OAuth ${credentials['oauth_token']}`
+            }
+        }
+    );
+    return response['data']['data']['currentUser']['dropCampaigns']
+}
+
+async function getDropCampaignDetails(credentials, dropId) {
+    const response = await axios.post('https://gql.twitch.tv/gql',
+        {
+            'operationName': 'DropCampaignDetails',
+            'extensions': {
+                'persistedQuery': {
+                    "version": 1,
+                    "sha256Hash": "14b5e8a50777165cfc3971e1d93b4758613fe1c817d5542c398dce70b7a45c05"
+                }
+            },
+            'variables': {
+                'dropID': dropId,
+                'channelLogin': credentials['channel_login']
+            }
+        },
+        {
+            headers: {
+                'Content-Type': 'text/plain;charset=UTF-8',
+                'Client-Id': credentials['client_id'],
+                'Authorization': `OAuth ${credentials['oauth_token']}`
+            }
+        }
+    );
+    return response['data']['data']['user']['dropCampaign']
+}
+
+async function getDropCampaignsInProgress(credentials) {
+    const inventory = await getInventory(credentials);
+    const campaigns = inventory['dropCampaignsInProgress'];
+    if (campaigns === null){
+        return [];
+    }
+    return campaigns;
+}
+
+async function getInventory(credentials) {
+    const response = await axios.post('https://gql.twitch.tv/gql',
+        {
+            'operationName': 'Inventory',
+            'extensions': {
+                "persistedQuery": {
+                    "version": 1,
+                    "sha256Hash": "9cdfc5ebf8ee497e49c5b922829d67e5bce039f3c713f0706d234944195d56ad"
+                }
+            }
+        },
+        {
+            headers: {
+                'Content-Type': 'text/plain;charset=UTF-8',
+                'Client-Id': credentials['client_id'],
+                'Authorization': `OAuth ${credentials['oauth_token']}`
+            }
+        }
+    );
+    return response['data']['data']['currentUser']['inventory'];
+}
+
+async function getDropEnabledStreams(credentials, game) {
+    const response = await axios.post('https://gql.twitch.tv/gql',
+        {
+            "operationName": "DirectoryPage_Game",
+            "variables": {
+                "name": game.toLowerCase(),
+                "options": {
+                    "includeRestricted": [
+                        "SUB_ONLY_LIVE"
+                    ],
+                    "sort": "RECENT",
+                    "recommendationsContext": {
+                        "platform": "web"
+                    },
+                    "requestID": "JIRA-VXP-2397", // TODO: what is this for???
+                    "tags": [
+                        "c2542d6d-cd10-4532-919b-3d19f30a768b"  // "Drops enabled"
+                    ]
+                },
+                "sortTypeIsRecency": true,
+                "limit": 30
+            },
+            "extensions": {
+                "persistedQuery": {
+                    "version": 1,
+                    "sha256Hash": "d5c5df7ab9ae65c3ea0f225738c08a36a4a76e4c6c31db7f8c4b8dc064227f9e"
+                }
+            }
+        },
+        {
+            headers: {
+                'Content-Type': 'text/plain;charset=UTF-8',
+                'Client-Id': credentials['client_id'],
+                'Authorization': `OAuth ${credentials['oauth_token']}`
+            }
+        }
+    );
+    const streams = response['data']['data']['game']['streams'];
+    if (streams === null){
+        return [];
+    }
+    const streamUrls = [];
+    for (const stream of streams['edges']){
+        streamUrls.push('https://www.twitch.tv/' + stream['node']['broadcaster']['login'])
+    }
+    return streamUrls;
+}
+
+async function claimDropReward(credentials, dropId) {
+    const response = await axios.post('https://gql.twitch.tv/gql',
+        {
+            "operationName": "DropsPage_ClaimDropRewards",
+            "variables": {
+                "input": {
+                    "dropInstanceID": dropId
+                }
+            },
+            "extensions": {
+                "persistedQuery": {
+                    "version": 1,
+                    "sha256Hash": "2f884fa187b8fadb2a49db0adc033e636f7b6aaee6e76de1e2bba9a7baf0daf6"
+                }
+            }
+        },
+        {
+            headers: {
+                'Content-Type': 'text/plain;charset=UTF-8',
+                'Client-Id': credentials['client_id'],
+                'Authorization': `OAuth ${credentials['oauth_token']}`
+            }
+        }
+    );
+    // TODO: verify success?
+    console.log(response);
+    //console.log(response['data']['data'])
+}
+
+module.exports = {
+    getDropCampaigns, getDropCampaignDetails, getDropCampaignsInProgress, getDropEnabledStreams, getInventory, claimDropReward
+}
